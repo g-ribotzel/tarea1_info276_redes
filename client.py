de0e8c6225ec9c92d6a2ef6efc524e3571b4bf0b
g-ribotzel <gstv.rr21@gmail.com>
2022-11-13 16:45:29 -0300

    Servidor con threads

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import inspect
 from ntp import *
 
-serverAddressPort   = ("172.25.40.131", 6000)
+serverAddressPort   = ("pool.ntp.org", 123)
 bufferSize          = 1024
 
 # Create a UDP socket at client side
@@ -16,14 +16,14 @@ msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 recvNTP = NTPPacket()
 recvNTP.from_data(msgFromServer[0])
 
-#Inspeccion de clase jaja :)
+#Inspeccion de clase
 attributes = inspect.getmembers(recvNTP, lambda a:not(inspect.isroutine(a)))
 msg = [a for a in attributes if not(a[0].startswith('__') and a[0].endswith('__'))]
-'''for x in msg:
+for x in msg:
     if(x[0]=='root_delay' or x[0]=='root_dispersion'):
         print(x[0], x[1]*(2**16))
     else:
-        print(x)'''
+        print(x)
 
 #timeNTP = recvNTP.tx_timestamp_high + float(recvNTP.tx_timestamp_low) / 2 ** 32 - NTP.NTP_DELTA
 timeNTP = recvNTP.tx_timestamp - NTP.NTP_DELTA
--- server.py
+++ server.py
@@ -1,34 +1,74 @@
+import threading
 import socket
-from _thread import *
-import os
+import time
+from ntp import *
 
+finalizar_g = False
+box = list()
 localIP     = "127.0.0.1"
 localPort   = 20001
-bufferSize  = 1024
-msgFromServer       = "Hello UDP Client"
-bytesToSend         = str.encode(msgFromServer)
+
+def recepcion_th(sock, caja):
+    global finalizar_g
+    print("Thread de recepcion inicializado")
+    while(not finalizar_g):
+        try:
+            data, address = sock.recvfrom(1024)
+            hora_recepcion = time.time()
+            print("RECEPCION > {0} \nTiempo: {1}".format(recv_msg, hora_recepcion))
+            caja.append((data, address, hora_recepcion))
+        except socket.timeout:
+            continue
+    print("Terminando thread de recepcion")
+
+def envio_th(sock, caja):
+    global finalizar_g
+    print("Thread de envios inicializado")
+    while(not finalizar_g):
+        if(len(caja) > 0):
+            data, address, recvTime = caja.pop(0)
+            recvNTP = NTPPacket()
+            recvNTP.from_data(data)
+
+            if(recvNTP.mode == 3):
+                sendNTP = NTPPacket(mode=4)
+            else:
+                sendNTP = NTPPacket(mode=2)
+            
+            sendNTP.stratum = 2
+            sednNTP.poll = recvNTP.poll
+            sendNTP.tx_timestamp = sendNTP.ref_timestamp = sendNTP.recv_timestamp = system_to_ntp_time(recvTime)
+            sendNTP.orig_timestamp = recvNTP.orig_timestamp
+
+            sock.sendto(sendNTP.to_data(), address)
+        else:
+            continue
+    print("Terminando thread de envios")
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.settimeout(5)
 
 # Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
-print("UDP server up and listening")
+print("Servidor UDP levantado y escuchando")
 
-# Listen for incoming datagrams
+recepcion = threading.Thread(target=recepcion_th, args=(UDPServerSocket, box,))
+recepcion.start()
+envio = threading.Thread(target=envio_th, args=(UDPServerSocket, box,))
+envio.start()
 
 while(True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    try:
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Exiting...")
 
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
+        finalizar_g = True
+        recepcion.join()
+        envio.join()
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    
-    print(clientMsg)
-    print(clientIP)
+        UDPServerSocket.close()
+        print("Exited")
+        break
 
-    # Sending a reply to client
-
-    UDPServerSocket.sendto(bytesToSend, address)
